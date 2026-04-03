289a4a83362a5cd3557f195564be5df29892042d
Luong Huu Thanh <117010350+fisherman611@users.noreply.github.com>
2026-04-03 14:09:07 +0700

    small update

--- scripts/qwen/sft/sft_qwen3_0.6B.sh
+++ scripts/qwen/sft/sft_qwen3_0.6B.sh
@@ -83,7 +83,7 @@ OPTS+=" --save ${SAVE_PATH}"
 OPTS+=" --seed ${SEED}"
 # deepspeed
 OPTS+=" --deepspeed"
-OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config_bf16.json"
+OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config_fp16.json"
 # type
 OPTS+=" --type lm"
 # generation
