f4c76c72fa5b38d06f6a5fe041b3e3f409906370
fisherman611 <thanhhuuluong11@gmail.com>
2026-04-08 18:54:44 +0000

    update some results

--- running.sh
+++ running.sh
@@ -2,4 +2,4 @@
 
 mkdir -p logs
 
-bash scripts/qwen/fdd/train_0.6B_4B_sfkl.sh >> log_qwen3_0.6B_4B_fdd_sfkl_kd_ratio_0.4.txt 2>&1
+bash scripts/qwen/fdd/train_0.6B_4B_srkl.sh >> log_qwen3_0.6B_4B_fdd_srkl.txt 2>&1
